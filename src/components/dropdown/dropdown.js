076ae0341bb310a2a0aba3a215365e8b134ce293
Lidia <ElidiyaE@yandex.ru>
2020-04-26 18:00:56 +0300

    dropdown can have different types: rooms and guests

--- src/components/dropdown/dropdown.js
+++ src/components/dropdown/dropdown.js
@@ -12,54 +12,68 @@ class Dropdown {
 	addDropdown() {
 		this.element.iqDropdown({
 
-			setSelectionText: (itemCount, totalItems) => { 
-				switch(this.type) {
-					case "rooms": 
-						return "2 спальни, 2 кровати, 0 ванных комнат";
-						break;
-					case "guests":	
-						return 'Сколько гостей';
-						break;
+			setSelectionText: (itemCount, totalItems) => {
+
+				if (totalItems === 0) {
+					return _getTextNoItems(this.type);
 				}
+				else {
+					let remain = _getRemain(totalItems);
+					return _getText(remain, totalItems, this.type);
+				}
+
 			},
 
 			onChange: (id, count, totalItems) => {
-				
-					let mult = 10;
-					while (mult < totalItems/10)
-						mult *= 10;
 
-					let remain = totalItems > 14 ? totalItems % mult : totalItems > 10 ?  0 : totalItems % mult;
-
-				this.elementText.text(totalItems + ' ' + _returnText(remain, totalItems));
+				let remain = _getRemain(totalItems);
+				this.elementText.text(_getText(remain, totalItems, this.type));
 			},
 
 		});
 
-		function _returnText (remain, totalItems) {
+		function _getRemain(totalItems) {
+			let mult = 10;
+
+			while (mult < totalItems/10)
+				mult *= 10;
+
+			if ( (totalItems > 14) || (totalItems < 11) )
+					return totalItems % mult;
+
+			return 0;
+		}
+
+		function _getTextNoItems(type) {
+			switch(type) {
+				case 'rooms': 
+					return 'Выберите значения';
+					break;
+				case 'guests':	
+					return 'Сколько гостей';
+					break;
+			}
+		}
+
+		function _getText(remain, totalItems, type) {
 
 			if (totalItems === 0) return 'Сколько гостей';
 
-			switch(remain) {
-				case 1:  
-					return 'гость';
+			switch(true) {
+				case remain === 1:  
+					totalItems += ' гость';
 					break;
 
-				case 2:
-				case 3:
-				case 4:
-					return 'гостя';
+				case remain < 5:
+					totalItems += ' гостя';
 					break;
 
-				case 5:
-				case 6:
-				case 7:
-				case 8:
-				case 9:
-				case 0:
-					return 'гостей';
+				default:
+					totalItems += ' гостей';
 					break;
 			}
+
+			return totalItems;
 		}
 	}
 
